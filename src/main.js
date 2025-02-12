5ba73ab80e93d2e3f6816d6c09e8ee8a32305f66
Ryan The <ryan.the.2006@gmail.com>
2025-02-12 18:06:34 +0800

    Port to npm and vite

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { Game as MainGame } from './scenes/Game';
+import { AUTO, Scale, Game } from 'phaser';
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config = {
+  type: AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: "#FFF",
+  parent: 'game-container',
+  scale: {
+    mode: Scale.FIT,
+    autoCenter: Scale.CENTER_BOTH
+  },
+  scene: [
+    MainGame
+  ],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+};
+
+export default new Game(config);
---
+++ src/scenes/Game.js
@@ -0,0 +1,21 @@
+import { Scene } from 'phaser';
+
+const WIDTH = 1024;
+const HEIGHT = 768;
+
+export class Game extends Scene {
+    constructor() {
+        super('Game');
+        this.player = null;
+    }
+
+    preload() {
+    }
+
+    create() {
+    }
+
+    update() {
+    }
+
+}
