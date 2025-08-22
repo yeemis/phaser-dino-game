808aa84bf37b4482c908ba22809b3d5fd3a83462
yeemis <yeemgame@gmail.com>
2025-08-22 05:06:50 +0000

    game over screen+collisions

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -24,13 +24,16 @@ export class Game extends Scene {
             this.load.image(`obstacle-${cactusNum}`,`assets/cactuses_${cactusNum}.png`);
             console.log(`loaded`)
         }
+        this.load.image("game-over","assets/game-over.png");
+        this.load.image("restart","assets/restart.png");
     }
 
     create() {
-        
+        this.isGameRunning = true;
+
         this.gameSpeed = 5;
         this.timer = 0;
-        this.player = this.physics.add.sprite(200,200,"dino")
+        this.player = this.physics.add.sprite(50 ,200,"dino")
             .setOrigin(0,1)
             .setGravityY(5000)
             .setCollideWorldBounds(true)
@@ -50,11 +53,20 @@ export class Game extends Scene {
             allowGravity: false,
         })
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(this.obstacles,this.player,this.gameOver,null,this);
+        this.gameOverText = this.add.image(0,0,"game-over");
+        this.restartText = this.add.image(0,80,"restart").setInteractive();
+            this.gameOverContainer = this.add
+                .container(1024/2,(300/2) - 50)
+                .add([this.gameOverText, this.restartText])
+                .setAlpha(0); //transparency
+
 
     
     }
 
     update(time, delta) {
+        if (!this.isGameRunning) {return;}
         this.ground.tilePositionX += this.gameSpeed;
         this.timer += delta;
         console.log(this.timer);
@@ -74,5 +86,21 @@ export class Game extends Scene {
         if (Phaser.Input.Keyboard.JustDown(space) || Phaser.Input.Keyboard.JustDown(up)&& this.player.body.onFloor()) {
             this.player.setVelocityY(-1600);
         }
+        this.restartText.on('pointerdown', () => {
+            this.physics.resume();
+            this.player.setVelocityY(0);
+            this.obstacles.clear(true, true);
+            this.gameOverContainer.setAlpha(0);
+            this.isGameRunning = true;
+        })
+        this.scoreText
     }
+    gameOver() {
+        this.physics.pause();
+        this.timer = 0;
+        this.isGameRunning = false;
+        this.gameOverContainer.setAlpha(1);
+    }
+    
 }
+ 
